226d8bc92cb30e5377cf0fe0169a43a5ee6d489a
Florian Brandel <mail@fbrandel.de>
2014-03-27 20:02:05 +0100

    Updated color mapping

--- test/HueColorsSpec.js
+++ test/HueColorsSpec.js
@@ -1,7 +1,7 @@
 var hueColors = require('../lib/HueLightStates');
 var expect = require('chai').expect;
 
-var states = ['FAILED', 'PASSED', 'RUNNING', 'DEACTIVATED'];
+var states = ['FAILED', 'PASSED', 'INSTABLE', 'DISABLED'];
 
 describe('HueLightStates', function () {
     it('should contain all build states represented by a hue lightState', function () {
