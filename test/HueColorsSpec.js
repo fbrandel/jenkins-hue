e1e6c292604e519a702a237c88134ecdfea3996c
Florian Brandel <mail@fbrandel.de>
2014-03-27 19:29:56 +0100

    Renamed HueColors

--- test/HueColorsSpec.js
+++ test/HueColorsSpec.js
@@ -1,9 +1,9 @@
-var hueColors = require('../lib/HueColors');
+var hueColors = require('../lib/HueLightStates');
 var expect = require('chai').expect;
 
 var states = ['FAILED', 'PASSED', 'RUNNING', 'DEACTIVATED'];
 
-describe('HueColors', function () {
+describe('HueLightStates', function () {
     it('should contain all build states represented by a hue lightState', function () {
         expect(hueColors).to.include.keys(states);
     });
